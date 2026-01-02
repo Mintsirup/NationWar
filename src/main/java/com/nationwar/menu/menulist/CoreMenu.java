dc22711dafb1a71c1df8b9930eb656f949bc7956
Mintsirup <sunjaewash@gmail.com>
2026-01-02 22:04:22 +0900

    코어 메뉴 수정

--- src/main/java/com/nationwar/menu/menulist/CoreMenu.java
+++ src/main/java/com/nationwar/menu/menulist/CoreMenu.java
@@ -12,9 +12,15 @@ public class CoreMenu {
     public void open(Player player) {
         Inventory inv = Bukkit.createInventory(null, 27, "코어 메뉴");
 
-        for (int i = 10; i <= 15; i++) {
-            inv.setItem(i, createItem(Material.BARRIER, "코어"));
-        }
+        inv.setItem(10, createItem(Material.BARRIER, "코어 1"));
+        inv.setItem(11, createItem(Material.BARRIER, "코어 2"));
+        inv.setItem(12, createItem(Material.BARRIER, "코어 3"));
+
+        // 가운데 13번 슬롯은 비움
+
+        inv.setItem(14, createItem(Material.BARRIER, "코어 4"));
+        inv.setItem(15, createItem(Material.BARRIER, "코어 5"));
+        inv.setItem(16, createItem(Material.BARRIER, "코어 6"));
 
         player.openInventory(inv);
     }
